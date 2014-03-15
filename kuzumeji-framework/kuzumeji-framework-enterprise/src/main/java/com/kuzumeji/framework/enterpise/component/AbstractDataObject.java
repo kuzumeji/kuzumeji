84c69a575130d24e645cf4df788474f56046af6f
nilcy <nilcy@mac.com>
2014-03-15 18:52:08 +0900

    OPEN - task 1: プロジェクトを方向付けする。 https://github.com/kuzumeji/kuzumeji/issues/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ kuzumeji-framework/kuzumeji-framework-enterprise/src/main/java/com/kuzumeji/framework/enterpise/component/AbstractDataObject.java
@@ -0,0 +1,45 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.kuzumeji.framework.enterpise.component;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+/**
+ * 基底データオブジェクト
+ * <dl>
+ * <dt>使用条件
+ * <dd>全データオブジェクトの基底クラスとすること。
+ * </dl>
+ * @author nilcy
+ */
+public abstract class AbstractDataObject implements DataObject {
+    /** 識別番号 */
+    private static final long serialVersionUID = 713428173593081041L;
+    /** 一時項目の検査 */
+    private static final boolean TEST_TRANSIENTS = true;
+    /** 一時項目の出力 */
+    private static final boolean OUTPUT_TRANSIENTS = true;
+    /** コンストラクタ */
+    public AbstractDataObject() {
+    }
+    /** {@inheritDoc} */
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this, TEST_TRANSIENTS);
+    }
+    /** {@inheritDoc} */
+    @Override
+    public boolean equals(final Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj, TEST_TRANSIENTS);
+    }
+    /** {@inheritDoc} */
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.DEFAULT_STYLE,
+            OUTPUT_TRANSIENTS);
+    }
+}
---
+++ kuzumeji-framework/kuzumeji-framework-enterprise/src/main/java/com/kuzumeji/framework/enterpise/component/DataObject.java
@@ -0,0 +1,17 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.kuzumeji.framework.enterpise.component;
+import java.io.Serializable;
+/**
+ * 基底データオブジェクトI/F
+ * <dl>
+ * <dt>使用条件
+ * <dd>全データオブジェクトの基底クラスのI/Fとすること。
+ * </dl>
+ * @author nilcy
+ */
+public interface DataObject extends Serializable {
+}
