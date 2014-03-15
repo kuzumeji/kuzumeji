35b64375aaaf06801f92e7ecda488a1c6c39cd77
nilcy <nilcy@mac.com>
2014-03-15 17:42:31 +0900

    OPEN - task 1: プロジェクトを方向付けする。 https://github.com/kuzumeji/kuzumeji/issues/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ kuzumeji-framework/kuzumeji-framework-standard/src/test/java/com/kuzumeji/framework/standard/LoggingTest.java
@@ -0,0 +1,25 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.kuzumeji.framework.standard;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * @see Hello
+ * @author nilcy
+ */
+@SuppressWarnings("all")
+public class LoggingTest {
+    private static final Logger LOG = LoggerFactory.getLogger(LoggingTest.class);
+    @Test
+    public void test() {
+        LOG.trace("[追跡] {},{},{}", "T1", "T2", "T3");
+        LOG.debug("[検査] {},{},{}", "D1", "D2", "D3");
+        LOG.info("[情報] {},{},{}", "I1", "I2", "I3");
+        LOG.warn("[警告] {},{},{}", "W1", "W2", "W3");
+        LOG.error("[例外] {},{},{}", "E1", "E2", "E3");
+    }
+}
