56a8aa8ba6a1b968e2c4a0670fc12861a73b5a6c
nilcy <nilcy@mac.com>
2014-03-15 17:11:28 +0900

    OPEN - task 1: プロジェクトを方向付けする。 https://github.com/kuzumeji/kuzumeji/issues/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ kuzumeji-framework/kuzumeji-framework-standard/src/main/java/com/kuzumeji/framework/standard/Hello.java
@@ -0,0 +1,23 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.kuzumeji.framework.standard;
+/**
+ * サンプルクラス
+ * @author nilcy
+ */
+public final class Hello {
+    /** 非公開コンストラクタ */
+    private Hello() {
+    }
+    /**
+     * サンプルメソッド
+     * @param name 名前
+     * @return こんにちは %s さん。
+     */
+    public static String sayHello(final String name) {
+        return String.format("こんにちは %s さん。", name);
+    }
+}
