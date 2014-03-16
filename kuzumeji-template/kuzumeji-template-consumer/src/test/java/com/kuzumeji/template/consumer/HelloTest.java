34a2c156aa333ed02fe9a4bbd579d5e096e29890
nilcy <nilcy@mac.com>
2014-03-16 15:04:02 +0900

    OPEN - task 1: プロジェクトを方向付けする。 https://github.com/kuzumeji/kuzumeji/issues/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ kuzumeji-template/kuzumeji-template-consumer/src/test/java/com/kuzumeji/template/consumer/HelloTest.java
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.kuzumeji.template.consumer;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.kuzumeji.template.consumer.Hello;
+/**
+ * @see Hello
+ * @author nilcy
+ */
+public class HelloTest {
+    @Test
+    public void testSayHello() {
+        assertThat(Hello.sayHello("nilcy"), is("こんにちは nilcy さん。"));
+    }
+}
