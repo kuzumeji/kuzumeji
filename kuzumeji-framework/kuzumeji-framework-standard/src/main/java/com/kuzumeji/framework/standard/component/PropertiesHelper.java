c72f588be9a868d05c3ee6b691795bd4ff1b3ea7
nilcy <nilcy@mac.com>
2014-03-21 16:32:02 +0900

    OPEN - task 1: プロジェクトを方向付けする。 https://github.com/kuzumeji/kuzumeji/issues/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ kuzumeji-framework/kuzumeji-framework-standard/src/main/java/com/kuzumeji/framework/standard/component/PropertiesHelper.java
@@ -0,0 +1,76 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.kuzumeji.framework.standard.component;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * コンフィグヘルパー
+ * @author nilcy
+ */
+public final class PropertiesHelper {
+    /** ロガー */
+    private static final Logger LOG = LoggerFactory.getLogger(PropertiesHelper.class);
+    /** コンフィグ */
+    private PropertiesConfiguration config;
+    /**
+     * コンストラクタ
+     * @param name プロパティ名
+     */
+    public PropertiesHelper(final String name) {
+        try {
+            config = new PropertiesConfiguration(name);
+        } catch (final ConfigurationException e) {
+            LOG.warn(e.getLocalizedMessage(), e);
+            config = null;
+        }
+    }
+    /**
+     * テキスト値の取得
+     * @param key キー
+     * @return テキスト値
+     */
+    public String getText(final String key) {
+        assert config != null;
+        return config.getString(key);
+    }
+    /**
+     * テキスト値の取得
+     * @param key キー
+     * @return テキスト値
+     */
+    public String[] getTexts(final String key) {
+        assert config != null;
+        return config.getStringArray(key);
+    }
+    /**
+     * 数値の取得
+     * @param key キー
+     * @return 数値
+     */
+    public int getNumeric(final String key) {
+        assert config != null;
+        return config.getInt(key);
+    }
+    /**
+     * プロパティ値の設定
+     * @param key キー
+     * @param value プロパティ値
+     */
+    public void setProperty(final String key, final Object value) {
+        assert config != null;
+        config.setProperty(key, value);
+    }
+    /**
+     * プロパティの保存
+     * @throws ConfigurationException コンフィグ例外
+     */
+    public void save() throws ConfigurationException {
+        assert config != null;
+        config.save();
+    }
+}
---
+++ kuzumeji-framework/kuzumeji-framework-standard/src/test/java/com/kuzumeji/framework/standard/component/PropertiesHelperTest.java
@@ -0,0 +1,38 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.kuzumeji.framework.standard.component;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import java.awt.Dimension;
+import org.apache.commons.configuration.ConfigurationException;
+import org.junit.Test;
+/**
+ * @see PropertiesHelper
+ * @author nilcy
+ */
+@SuppressWarnings("all")
+public class PropertiesHelperTest {
+    @Test
+    public final void testSimple() throws ConfigurationException {
+        final PropertiesHelper testee = new PropertiesHelper("usergui.properties");
+        assertThat(testee, is(not(nullValue())));
+        assertThat(testee.getText("colors.background"), is("#FFFFFF"));
+        assertThat(testee.getText("colors.foreground"), is("#000080"));
+        assertThat(
+            new Dimension(testee.getNumeric("window.width"), testee.getNumeric("window.height")),
+            is(not(nullValue())));
+        testee.setProperty("colors.foreground", "#000000");
+        assertThat(testee.getText("colors.foreground"), is("#000000"));
+        // testee.save();
+        assertThat(testee.getText("key"), is("This \n string \t contains , escaped \\ characters"));
+    }
+    @Test
+    public final void testVariable() throws ConfigurationException {
+        final PropertiesHelper testee = new PropertiesHelper("application.properties");
+        assertThat(testee, is(not(nullValue())));
+        assertThat(testee.getText("application.title"), is("Kuzumeji Framework 0.1.0-SNAPSHOT"));
+    }
+}
