44d7fffeddcb39ec6beb58e0bd74ad5c17f393ae
nilcy <nilcy@mac.com>
2014-03-31 17:49:55 +0900

    OPEN - task 1: プロジェクトを方向付けする。 https://github.com/kuzumeji/kuzumeji/issues/issues/issue/1

--- kuzumeji-framework/kuzumeji-framework-enterprise/src/test/java/com/kuzumeji/framework/enterprise/component/persistence/PersistableTesteeTest.java
+++ kuzumeji-framework/kuzumeji-framework-enterprise/src/test/java/com/kuzumeji/framework/enterprise/component/persistence/PersistableTesteeTest.java
@@ -7,7 +7,6 @@ package com.kuzumeji.framework.enterprise.component.persistence;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import java.util.Collection;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -17,17 +16,13 @@ import org.jboss.arquillian.transaction.api.annotation.Transactional;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import com.kuzumeji.framework.testing.ArchiveFactory;
 /**
  * @author nilcy
  */
 @RunWith(Arquillian.class)
 @Transactional(value = TransactionMode.ROLLBACK)
-@SuppressWarnings("all")
 public class PersistableTesteeTest {
-    @Inject
-    private Logger log;
     @PersistenceContext
     private EntityManager manager;
     @Deployment
@@ -42,7 +37,7 @@ public class PersistableTesteeTest {
         entity.setName("name#01");
         manager.persist(entity);
         final Collection<PersistableTestee> entities = manager.createQuery(
-            "select pt from PersistableTestee pt").getResultList();
+            "select pt from PersistableTestee pt", PersistableTestee.class).getResultList();
         assertThat(entities.size(), is(1));
         final PersistableTestee first = entities.iterator().next();
         assertThat(first.getCode(), is("code#01"));
