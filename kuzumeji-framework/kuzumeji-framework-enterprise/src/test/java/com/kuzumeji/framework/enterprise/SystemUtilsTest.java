1b22e637d5284eb157c89a4a009ff8dcd861a05b
nilcy <nilcy@mac.com>
2014-04-09 16:27:41 +0900

    OPEN - task 1: プロジェクトを方向付けする。 https://github.com/kuzumeji/kuzumeji/issues/issues/issue/1

---
+++ kuzumeji-framework/kuzumeji-framework-enterprise/src/test/java/com/kuzumeji/framework/enterprise/SystemUtilsTest.java
@@ -0,0 +1,21 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.kuzumeji.framework.enterprise;
+import org.apache.commons.lang3.SystemUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * @see SystemUtils
+ * @author nilcy
+ */
+public class SystemUtilsTest {
+    private static final Logger LOG = LoggerFactory.getLogger(SystemUtilsTest.class);
+    @Test
+    public void test() {
+        LOG.debug("{}", SystemUtils.JAVA_IO_TMPDIR);
+    }
+}
