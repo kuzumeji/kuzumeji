69fcc1f21b2930ce245556aec719211bd01da42f
nilcy <nilcy@mac.com>
2014-04-11 15:38:13 +0900

    OPEN - task 1: プロジェクトを方向付けする。 https://github.com/kuzumeji/kuzumeji/issues/issues/issue/1

---
+++ kuzumeji-template/kuzumeji-template-consumer/src/main/java/com/kuzumeji/template/consumer/rest/RestApplication.java
@@ -0,0 +1,18 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Kuzumeji Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.kuzumeji.template.consumer.rest;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+/**
+ * RESTアプリケーション
+ * @author nilcy
+ */
+@ApplicationPath("/rest/*")
+public class RestApplication extends Application {
+    /** コンストラクタ */
+    public RestApplication() {
+    }
+}
