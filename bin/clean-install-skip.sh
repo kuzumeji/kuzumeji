9c68a535c1f62b1ccf4efdb95ee8ab3ce1e56974
nilcy <nilcy@mac.com>
2014-04-12 12:55:26 +0900

    OPEN - task 1: プロジェクトを方向付けする。 https://github.com/kuzumeji/kuzumeji/issues/issues/issue/1
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ bin/clean-install-skip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+KUZUMEJI_HOME=`dirname $0`/..
+for d in kuzumeji-root kuzumeji-framework kuzumeji-template;
+do
+    (cd "$KUZUMEJI_HOME/$d"; mvn clean install -Dmaven.test.skip=true;)
+done;
