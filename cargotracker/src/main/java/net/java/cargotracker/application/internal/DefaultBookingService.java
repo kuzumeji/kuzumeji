de7d6821a93df33303667aa45746f011833be4ba
nilcy <nilcy@mac.com>
2014-06-30 10:35:44 +0900

    Signed-off-by: nilcy <nilcy@mac.com>

--- cargotracker/src/main/java/net/java/cargotracker/application/internal/DefaultBookingService.java
+++ cargotracker/src/main/java/net/java/cargotracker/application/internal/DefaultBookingService.java
@@ -1,5 +1,4 @@
 package net.java.cargotracker.application.internal;
-
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -17,10 +16,18 @@ import net.java.cargotracker.domain.model.location.Location;
 import net.java.cargotracker.domain.model.location.LocationRepository;
 import net.java.cargotracker.domain.model.location.UnLocode;
 import net.java.cargotracker.domain.service.RoutingService;
-
+/**
+ * 予約サービス
+ * <dl>
+ * <dt>表明保証
+ * <dd>
+ * <dt>特記など
+ * <dd>
+ * </dl>
+ * @author nilcy
+ */
 @Stateless
 public class DefaultBookingService implements BookingService {
-
     @Inject
     private CargoRepository cargoRepository;
     @Inject
@@ -28,62 +35,45 @@ public class DefaultBookingService implements BookingService {
     @Inject
     private RoutingService routingService;
     // TODO See if the logger can be injected.
-    private static final Logger logger = Logger.getLogger(
-            DefaultBookingService.class.getName());
-
+    private static final Logger logger = Logger.getLogger(DefaultBookingService.class.getName());
     @Override
-    public TrackingId bookNewCargo(UnLocode originUnLocode,
-            UnLocode destinationUnLocode,
-            Date arrivalDeadline) {
-        TrackingId trackingId = cargoRepository.nextTrackingId();
-        Location origin = locationRepository.find(originUnLocode);
-        Location destination = locationRepository.find(destinationUnLocode);
-        RouteSpecification routeSpecification = new RouteSpecification(origin,
-                destination, arrivalDeadline);
-
-        Cargo cargo = new Cargo(trackingId, routeSpecification);
-
+    public TrackingId bookNewCargo(final UnLocode originUnLocode,
+        final UnLocode destinationUnLocode, final Date arrivalDeadline) {
+        final TrackingId trackingId = cargoRepository.nextTrackingId();
+        final Location origin = locationRepository.find(originUnLocode);
+        final Location destination = locationRepository.find(destinationUnLocode);
+        final RouteSpecification routeSpecification = new RouteSpecification(origin, destination,
+            arrivalDeadline);
+        final Cargo cargo = new Cargo(trackingId, routeSpecification);
         cargoRepository.store(cargo);
-        logger.log(Level.INFO, "Booked new cargo with tracking id {0}",
-                cargo.getTrackingId().getIdString());
-
+        logger.log(Level.INFO, "Booked new cargo with tracking id {0}", cargo.getTrackingId()
+            .getIdString());
         return cargo.getTrackingId();
     }
-
     @Override
-    public List<Itinerary> requestPossibleRoutesForCargo(TrackingId trackingId) {
-        Cargo cargo = cargoRepository.find(trackingId);
-
+    public List<Itinerary> requestPossibleRoutesForCargo(final TrackingId trackingId) {
+        final Cargo cargo = cargoRepository.find(trackingId);
         if (cargo == null) {
             return Collections.emptyList();
         }
-
         return routingService.fetchRoutesForSpecification(cargo.getRouteSpecification());
     }
-
     @Override
-    public void assignCargoToRoute(Itinerary itinerary, TrackingId trackingId) {
-        Cargo cargo = cargoRepository.find(trackingId);
-
+    public void assignCargoToRoute(final Itinerary itinerary, final TrackingId trackingId) {
+        final Cargo cargo = cargoRepository.find(trackingId);
         cargo.assignToRoute(itinerary);
         cargoRepository.store(cargo);
-
         logger.log(Level.INFO, "Assigned cargo {0} to new route", trackingId);
     }
-
     @Override
-    public void changeDestination(TrackingId trackingId, UnLocode unLocode) {
-        Cargo cargo = cargoRepository.find(trackingId);
-        Location newDestination = locationRepository.find(unLocode);
-
-        RouteSpecification routeSpecification = new RouteSpecification(
-                cargo.getOrigin(), newDestination,
-                cargo.getRouteSpecification().getArrivalDeadline());
+    public void changeDestination(final TrackingId trackingId, final UnLocode unLocode) {
+        final Cargo cargo = cargoRepository.find(trackingId);
+        final Location newDestination = locationRepository.find(unLocode);
+        final RouteSpecification routeSpecification = new RouteSpecification(cargo.getOrigin(),
+            newDestination, cargo.getRouteSpecification().getArrivalDeadline());
         cargo.specifyNewRoute(routeSpecification);
-
         cargoRepository.store(cargo);
-
-        logger.log(Level.INFO, "Changed destination for cargo {0} to {1}",
-                new Object[]{trackingId, routeSpecification.getDestination()});
+        logger.log(Level.INFO, "Changed destination for cargo {0} to {1}", new Object[] {
+            trackingId, routeSpecification.getDestination() });
     }
 }
--- cargotracker/src/main/java/net/java/cargotracker/domain/shared/Specification.java
+++ cargotracker/src/main/java/net/java/cargotracker/domain/shared/Specification.java
@@ -1,44 +1,40 @@
 package net.java.cargotracker.domain.shared;
-
 /**
+ * 仕様オブジェクトI/F
+ * <dl>
+ * <dt>表明保証
+ * <dd>仕様を作成するための基本として、{@link net.java.cargotracker.domain.shared.AbstractSpecification}を使用して
+ * <dt>特記など
+ * <dd>
+ * </dl>
  * Specification interface.
  * <p/>
- * Use {@link net.java.cargotracker.domain.shared.AbstractSpecification} as base
- * for creating specifications, and only the method
- * {@link #isSatisfiedBy(Object)} must be implemented.
+ * Use {@link net.java.cargotracker.domain.shared.AbstractSpecification} as base for creating
+ * specifications, and only the method {@link #isSatisfiedBy(Object)} must be implemented.
  */
 public interface Specification<T> {
-
     /**
      * Check if {@code t} is satisfied by the specification.
-     *
      * @param t Object to test.
      * @return {@code true} if {@code t} satisfies the specification.
      */
     boolean isSatisfiedBy(T t);
-
     /**
-     * Create a new specification that is the AND operation of {@code this}
-     * specification and another specification.
-     *
+     * Create a new specification that is the AND operation of {@code this} specification and
+     * another specification.
      * @param specification Specification to AND.
      * @return A new specification.
      */
     Specification<T> and(Specification<T> specification);
-
     /**
-     * Create a new specification that is the OR operation of {@code this}
-     * specification and another specification.
-     *
+     * Create a new specification that is the OR operation of {@code this} specification and another
+     * specification.
      * @param specification Specification to OR.
      * @return A new specification.
      */
     Specification<T> or(Specification<T> specification);
-
     /**
-     * Create a new specification that is the NOT operation of {@code this}
-     * specification.
-     *
+     * Create a new specification that is the NOT operation of {@code this} specification.
      * @param specification Specification to NOT.
      * @return A new specification.
      */
